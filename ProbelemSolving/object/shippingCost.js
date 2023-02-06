90799de97345ecad138acc3d516fb3dd678387af
jabed366 <jabedhasan231@gmail.com>
2023-02-06 18:52:16 +0600

    TotalProductPrice

---
+++ ProbelemSolving/object/shippingCost.js
@@ -0,0 +1,34 @@
+let cartProducts=[
+    {
+        name: 'samsung s5',
+        price: 45000,
+        camera: 10,
+        storage: 32
+    },
+    {
+        name: 'walton m32',
+        price: 15000,
+        camera: 8,
+        storage: 16
+    },
+    {
+        name: 'nokia n95',
+        price: 8000,
+        camera: 5,
+        storage: 8
+    }
+]
+
+
+function total(cartProducts){
+    let total=0;
+    for(let i=0;i<cartProducts.length;i++){
+        total=total+cartProducts[i].price;
+    }
+    return total;
+}
+
+
+const total_price=total(cartProducts);
+
+console.log(total_price);
